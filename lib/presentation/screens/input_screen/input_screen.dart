5daf3130834adcb74e5c51ebf86b6c85a88ef3cb
ArifAppDev <arifgt77@gmail.comm>
2026-01-30 23:06:49 +0600

    arif

--- lib/presentation/screens/input_screen/input_screen.dart
+++ lib/presentation/screens/input_screen/input_screen.dart
@@ -54,7 +54,7 @@ class _InputScreenState extends State<InputScreen> {
             ),
           ),
 
-          // second container ....weight...............
+          // second container ============weight ===============
           Container(
             margin: EdgeInsets.only(top: 10, right: 55, left: 55),
             padding: EdgeInsets.symmetric(vertical: 30, horizontal: 10),
